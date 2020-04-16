f9fcc61d5e4363db8812b49652088185cede7a2f
Nick Barone <xnickbarone@gmail.com>
2020-04-16 01:55:31 -0400

    Add bash script

--- import-project-files.sh
+++ import-project-files.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
-origin_dir="~/repos/cra-project-files"
-echo $PWD
+origin_dir="/Users/$USER/repos/cra-project-files"
+cp $origin_dir/.eslintrc $PWD
+cp $origin_dir/.prettierrc $PWD
+cp $origin_dir/jsconfig.json $PWD
+mkdir .vscode
+cp $origin_dir/.vscode/settings.json $PWD/.vscode/settings.json
 
-cp "$origin_dir/.eslintrc" $PWD
+echo "Custom settings files copied to $PWD"
+
+echo "Adding Yarn dependencies"
+yarn add --dev babel-eslint eslint eslint-config-prettier eslint-plugin-html eslint-plugin-prettier eslint-plugin-react eslint-plugin-react-hooks prettier
+yarn add eslint-plugin-babel
+
+echo "Running Prettier"
+npx prettier --loglevel error --write .
+
+echo "Committing preference files"
+git add .
+git commit -m "Add preference files, install default deps, run prettier on all files (automatic commit via init-cra)"
