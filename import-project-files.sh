5a58a5f6f29a7b5711dd16c6c73916e79b008d37
Nick Barone <xnickbarone@gmail.com>
2020-05-13 11:27:05 -0400

    Add server scaffolding

--- import-project-files.sh
+++ import-project-files.sh
@@ -8,11 +8,14 @@ cp $origin_dir/.prettierrc $PWD
 cp $origin_dir/jsconfig.json $PWD
 mkdir .vscode
 cp $origin_dir/.vscode/settings.json $PWD/.vscode/settings.json
+mkdir server
+mkdir server/routes
+mkdir server/lib
 
 
 # add yarn deps
 echo "Adding Yarn dependencies"
-yarn add --dev babel-eslint eslint eslint-plugin-babel eslint-config-prettier eslint-plugin-html eslint-plugin-prettier eslint-plugin-react eslint-plugin-react-hooks prettier
+yarn add --dev babel-eslint eslint eslint-plugin-babel eslint-config-prettier eslint-plugin-html eslint-plugin-prettier eslint-plugin-react eslint-plugin-react-hooks prettier express minimist body-parser
 yarn add styled-components
 
 # lock packages
@@ -26,3 +29,8 @@ npx prettier --loglevel error --write .
 echo "Committing preference files"
 git add .
 git commit -m "Add preference files, install default deps, run prettier on all files (automatic commit via init-cra)"
+
+echo "************"
+echo "Don't forget to proxy your server in package.json"
+echo "\"proxy\": \"http://localhost:8080\","
+echo "************"
---
+++ server/index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var argv = require('minimist')(process.argv.slice(2));
+var bodyParser = require('body-parser');
+
+var app = express();
+app.enable('trust proxy');
+
+/**
+ * Set JSON response type
+ */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+	res.setHeader('Content-Type', 'application/json');
+	next();
+});
+
+/**
+ * Set middleware for delay
+ */
+const delay = argv['delay'] ? argv['delay'] : 500;
+app.use((req, res, next) => setTimeout(next, delay));
+
+/*
+ * Enable CORS
+ */
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
+
+/**
+ * Load routes
+ */
+var routesAuth = require('./routes/index.js');
+
+app.use('/api/v1/index', routeIndex);
+
+/**
+ * Start server
+ */
+var serverPort = argv['port'] !== undefined ? argv['port'] : 8080;
+
+app.listen(serverPort, function () {
+	console.log(`mock-server started on ${serverPort} with ${delay}ms artificial delay`);
+});
