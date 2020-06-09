10c11705b0421031ce3e0dded4d61c07633e691d
Nick Barone <xnickbarone@gmail.com>
2020-06-08 21:42:10 -0400

    Simplify logic for server init, add --no-commit flag, update README

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # CRA Project Files
 
-This repo is for my own personal CRA app config. If you would like to use this, you can install it and add `alias init_cra="sh ~/repos/cra-project-files/import-project-files.sh"` to your startup script (.bashrc, .zshrc, etc)
+This repo is for my own personal CRA app config. If you would like to use this, you can install it and add `alias init-cra="sh ~/repos/cra-project-files/import-project-files.sh"` to your startup script (.bashrc, .zshrc, etc)
 
 You will need to modify `origin_dir` in `import-project-files.sh` to point to your own directory.
+
+
+## Options
+
+### ***-s / --server***
+
+Applying this flag will install `express` and `body-parser`, along with creating a server folder and a basic Express API scaffold. This will also update your `package.json` file to start the server and proxy it to your CRA app.
+
+### ***-nc / --no-commit***
+
+Applying this flag will skip automatically committing the files created and altered by init-cra
+
--- import-project-files.sh
+++ import-project-files.sh
@@ -3,64 +3,75 @@
 PURPLE='\033[0;35m'
 NC='\033[0m'
 SERVER_DEPS=0
+NO_COMMIT=0
+ORIGIN_DIR="/Users/$USER/repos/cra-project-files"
 
-# copying files
-echo "${PURPLE}Copying custom settings files to $PWD${NC}"
-origin_dir="/Users/$USER/repos/cra-project-files"
-#client
-cp $origin_dir/.eslintrc $PWD
-cp $origin_dir/.prettierrc $PWD
-cp $origin_dir/jsconfig.json $PWD
-cp $origin_dir/.env $PWD
-mkdir .vscode
-cp $origin_dir/.vscode/settings.json $PWD/.vscode/settings.json
-
-
-# add yarn deps
-echo "${PURPLE}Adding React dependencies${NC}"
-yarn add --dev babel-eslint eslint eslint-plugin-babel eslint-config-prettier eslint-plugin-html eslint-plugin-prettier eslint-plugin-react eslint-plugin-react-hooks prettier
-yarn add styled-components
-
-# check for server flag and add deps
+# Check for server flag and add deps
 for arg in "$@"
 do
 	case $arg in
 		-s|--server)
-		SERVER_DEPS=1
+			SERVER_DEPS=1
+			shift
+			;;
 
-		# add json global dep
-		package="json"
-		if [ `npm list -g --depth=0 --silent | grep -c $package` -eq 0 ]; then
-			echo "${PURPLE}Installing json package globally via npm${NC}"
-			npm install -g $package --no-shrinkwrap
-		fi
-
-		echo "${PURPLE}Adding server dependencies${NC}"
-		yarn add express body-parser
-		mkdir server
-		mkdir server/routes
-		mkdir server/lib
-		cp $origin_dir/server/index.js $PWD/server/index.js
-		cp $origin_dir/server/routes/index.js $PWD/server/routes/index.js
-		shift
-		;;
+		-nc|--no-commit)
+			NO_COMMIT=1
+			shift
+			;;
 	esac
 done
 
-# lock packages
+# Copy config files
+echo "${PURPLE}Copying custom settings files to $PWD${NC}"
+cp $ORIGIN_DIR/.eslintrc $PWD
+cp $ORIGIN_DIR/.prettierrc $PWD
+cp $ORIGIN_DIR/jsconfig.json $PWD
+cp $ORIGIN_DIR/.env $PWD
+mkdir .vscode
+cp $ORIGIN_DIR/.vscode/settings.json $PWD/.vscode/settings.json
+
+
+# Add front-end deps
+echo "${PURPLE}Adding React dependencies${NC}"
+yarn add --dev babel-eslint eslint eslint-plugin-babel eslint-config-prettier eslint-plugin-html eslint-plugin-prettier eslint-plugin-react eslint-plugin-react-hooks prettier
+yarn add styled-components
+
+# Lock packages
 sed -i '' 's/\"\^/\"/g' package.json
 
 if [ $SERVER_DEPS -eq 1 ]; then
+	# Add json global dep
+	package="json"
+	if [ `npm list -g --depth=0 --silent | grep -c $package` -eq 0 ]; then
+		echo "${PURPLE}Installing json package globally via npm${NC}"
+		npm install -g $package --no-shrinkwrap
+	fi
+
+	# Add server deps
+	echo "${PURPLE}Adding server dependencies${NC}"
+	yarn add express body-parser
+	mkdir server
+	mkdir server/routes
+	mkdir server/lib
+	cp $ORIGIN_DIR/server/index.js $PWD/server/index.js
+	cp $ORIGIN_DIR/server/routes/index.js $PWD/server/routes/index.js
+
+	# Update package.json for server config
 	echo "${PURPLE}Updating package.json for server initialization${NC}"
 	json -I -f $PWD/package.json -e 'this.proxy="http://localhost:8080"'
 	json -I -f $PWD/package.json -e 'this.scripts.start="nodemon server/index.js & react-scripts start"'
 fi
 
-# run prettier
+# Run prettier
 echo "${PURPLE}Running Prettier${NC}"
 npx prettier --loglevel error --write .
 
-# commit files
-echo "${PURPLE}Committing preference files${NC}"
-git add .
-git commit -m "Add preference files, install default deps, run prettier on all files (automatic commit via init-cra)"
+# Commit files
+if [ $NO_COMMIT -eq 0 ]; then
+	echo "${PURPLE}Committing preference files${NC}"
+	git add .
+	git commit -m "Add preference files, install default deps, run prettier on all files (automatic commit via init-cra)"
+else
+	echo "${PURPLE}No Commit flag found, skipping automatic commit${NC}"
+fi
